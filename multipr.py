c1042c1255e16927f405ebd5d2e466393014a363
shibaji7 <shibaji7@vt.edu>
2018-02-23 23:09:14 -0500

    Multi Process

--- multipr.py
+++ multipr.py
@@ -5,12 +5,13 @@ import pandas as pd
 out = "out_sza"
 
 def run_code(l):
-    cmd = "python model.py %.15f %.15f %.15 %.15f %s" % (l[0],l[1],l[3],l[4],out)
+    #print type(l[0]),type(l[1]),type(l[]
+    cmd = "python model.py %.15f %.15f %.15f %.15f %s" % (float(l[0]),float(l[1]),float(l[3]),float(l[4]), out)
     print cmd
-    os.system(cmd)
+    #os.system(cmd)
     return
 
-D = pd.read_csv("R/out/design.csv")
+D = pd.read_csv("R/out/design_sza.csv")
 d = D.as_matrix()
 
 pool = mpr.Pool(10)
